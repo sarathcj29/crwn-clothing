1c9b57bfd497a5bcc7780a09f232fe69a9586746
sarathcj29 <sarathcj29@gmail.com>
2022-10-22 22:42:19 +0530

    Added empty message into cart dropdown and scroll bar issue fixed

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,9 @@ import './cart-dropdown.styles.scss';
 const CartDropDown = () => {
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        <span className="empty-message">Your cart is empty</span>
+      </div>
       <Button>Go To Checkout</Button>
     </div>
   );
